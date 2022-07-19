6d02043dff0fd00921fb611603b1079e514fd181
Jacob Maine <jacob.maine@gmail.com>
2022-07-19 15:52:23 -0700

    Further simplify tracing

--- src/lsp4clj/trace.clj
+++ src/lsp4clj/trace.clj
@@ -8,14 +8,11 @@
   (format "[Trace - %s]"
           (str (.truncatedTo at java.time.temporal.ChronoUnit/MILLIS))))
 
-(defn ^:private format-request-header [{:keys [method id]}]
-  (format "request '%s - (%s)'" method id))
+(defn ^:private format-request-signature [{:keys [method id]}]
+  (format "'%s - (%s)'" method id))
 
-(defn ^:private format-response-header [{:keys [method id]}]
-  (format "response '%s - (%s)'" method id))
-
-(defn ^:private format-notification-header [{:keys [method]}]
-  (format "notification '%s'" method))
+(defn ^:private format-notification-signature [{:keys [method]}]
+  (format "'%s'" method))
 
 (defn ^:private format-body [label body]
   (str label ": " (json/generate-string body {:pretty true})))
@@ -28,26 +25,26 @@
     (format-body "Error data" (:data error))
     (format-body "Result" result)))
 
-(defn ^:private format-str [at action header body]
-  (str (format-tag at) " " action " " header "\n"
+(defn ^:private format-str [at action message-type header-details body]
+  (str (format-tag at) " " action " " message-type " " header-details "\n"
        body "\n\n\n"))
 
 (defn ^:private latency [^java.time.Instant started ^java.time.Instant finished]
   (format "%sms" (- (.toEpochMilli finished) (.toEpochMilli started))))
 
 (defn ^:private format-notification [action notif at]
-  (format-str at action (format-notification-header notif)
+  (format-str at action "notification" (format-notification-signature notif)
               (format-params notif)))
 
 (defn ^:private format-request [action req at]
-  (format-str at action (format-request-header req)
+  (format-str at action "request" (format-request-signature req)
               (format-params req)))
 
 (defn ^:private format-response [action req {:keys [error] :as resp} started finished]
-  (format-str finished action
+  (format-str finished action "response"
               (format
                 (str "%s. Request took %s." (when error " Request failed: %s (%s)."))
-                (format-response-header req)
+                (format-request-signature req)
                 (latency started finished)
                 (:message error) (:code error))
               (format-response-body resp)))
@@ -57,7 +54,7 @@
 (defn received-response [req resp started finished] (format-response "Received" req resp started finished))
 
 (defn received-unmatched-response [resp at]
-  (format-str at "Received" "response for unmatched request:"
+  (format-str at "Received" "response" "for unmatched request:"
               (format-body "Body" resp)))
 
 (defn sending-notification [notif at] (format-notification "Sending" notif at))
